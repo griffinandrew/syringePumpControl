88a317e54e08ef6591c0c68270ac4d8d11c1b183
griffinandrew <gaheyrich@gmail.com>
2022-02-08 17:21:44 -0500

    adding thread tasks

--- oldCode/Jacob_Backend.py
+++ oldCode/Jacob_Backend.py
@@ -1,6 +1,7 @@
 from Jacob_Pump import Pump
 from serial import Serial
-import multithreading
+#import multithreading
+import threading
 
 Pump1 = Pump("COM3","11 PICO PLUS ELITE 3.0.7")
 Pump2 = Pump("COM4","11 PICO PLUS ELITE 3.0.7")
@@ -22,38 +23,26 @@ class Backend:
         self.curVal2 = 0
 
         self.oldVal3 = 0
-        self.curVal3 = 0        
+        self.curVal3 = 0
 
-    def buttonPush(self):
 
-        # Slide all the current slider values to the old slider values
+
+    # i believe that if i create a thread for each of pump 1-3 actions it should work concurrently
+
+    def pump_1_thread_task(self):
         self.oldVal1 = self.curVal1
-        self.oldVal2 = self.curVal2
-        self.oldVal3 = self.curVal3
 
-        # Set all current slider values equal to values from the GUI
         self.curVal1 = self.gui.c1.get()
-        self.curVal2 = self.gui.c2.get()
-        self.curVal3 = self.gui.c3.get()
 
-        # Set the values for percent change in volume from each slider
         self.deltVal1 = self.curVal1 - self.oldVal1
-        self.deltVal2 = self.curVal2 - self.oldVal2
-        self.deltVal3 = self.curVal3 - self.oldVal3
 
-        # Multiply by the max volume per channel and divide by 100 to get the absolute
-        # change in volume for each channel
         self.deltVol1 = 0.01 * self.deltVal1 * float(self.gui.maxvol_entry.get())
-        self.deltVol2 = 0.01 * self.deltVal2 * float(self.gui.maxvol_entry.get())
-        self.deltVol3 = 0.01 * self.deltVal3 * float(self.gui.maxvol_entry.get())
 
-        # Define other GUI parameters
+        print(str(self.deltVol1))
+
         self.rate = float(self.gui.rate_entry.get())
         self.vol = float(self.gui.vol_entry.get())
         self.diam = float(self.gui.diam_entry.get())
-
-        # Print volume changes for each channel as a sanity check
-        print(str(self.deltVol1), str(self.deltVol2), str(self.deltVol3))
 
         # Set the pump parameters for Pump 1
         Pump1.c_volume()
@@ -65,6 +54,28 @@ class Backend:
         Pump1.withdraw_rate(str(self.rate), "ml/min")
         Pump1.target_volume(str(abs(self.deltVol1)), "ml")
 
+        if self.deltVol1 < 0:
+            Pump1.withdraw_pump()
+        elif self.deltVol1 > 0:
+            Pump1.infuse_pump()
+        else:
+            Pump1.stop_pump()
+
+    def pump_2_thread_task(self):
+        self.oldVal2 = self.curVal2
+
+        self.curVal2 = self.gui.c2.get()
+
+        self.deltVal2 = self.curVal2 - self.oldVal2
+
+        self.deltVol2 = 0.01 * self.deltVal2 * float(self.gui.maxvol_entry.get())
+
+        print(str(self.deltVol2))
+
+        self.rate = float(self.gui.rate_entry.get())
+        self.vol = float(self.gui.vol_entry.get())
+        self.diam = float(self.gui.diam_entry.get())
+
         # Set the pump parameters for Pump 2
         Pump2.c_volume()
         Pump2.ci_volume()
@@ -74,6 +85,26 @@ class Backend:
         Pump2.infuse_rate(str(self.rate), "ml/min")
         Pump2.withdraw_rate(str(self.rate), "ml/min")
         Pump2.target_volume(str(abs(self.deltVol2)), "ml")
+
+        if self.deltVol2 < 0:
+            Pump2.withdraw_pump()
+        elif self.deltVol2 > 0:
+            Pump2.infuse_pump()
+        else:
+            Pump2.stop_pump()
+
+    def pump_3_thread_task(self):
+        self.oldVal3 = self.curVal3
+
+        self.curVal3 = self.gui.c3.get()
+
+        self.deltVal3 = self.curVal3 - self.oldVal3
+
+        print(str(self.deltVol3))
+
+        self.rate = float(self.gui.rate_entry.get())
+        self.vol = float(self.gui.vol_entry.get())
+        self.diam = float(self.gui.diam_entry.get())
 
         # Set the pump parameters for Pump 3
         Pump3.c_volume()
@@ -85,21 +116,6 @@ class Backend:
         Pump3.withdraw_rate(str(self.rate), "ml/min")
         Pump3.target_volume(str(abs(self.deltVol3)), "ml")
 
-        # Actuate each pump
-        if self.deltVol1 < 0:
-            Pump1.withdraw_pump()
-        elif self.deltVol1 > 0:
-            Pump1.infuse_pump()
-        else:
-            Pump1.stop_pump()
-
-        if self.deltVol2 < 0:
-            Pump2.withdraw_pump()
-        elif self.deltVol2 > 0:
-            Pump2.infuse_pump()
-        else:
-            Pump2.stop_pump()
-            
         if self.deltVol3 < 0:
             Pump3.withdraw_pump()
         elif self.deltVol3 > 0:
@@ -108,17 +124,20 @@ class Backend:
             Pump3.stop_pump()
 
 
-    # i believe that if i create a thread for each of pump 1-3 actions it should work concurrently
+    def buttonPush(self):
+        pump1_thread = threading.Thread(target=thread_task)
+        pump2_thread = threading.Thread(target=pump_2_thread_task)
+        pump3_thread = threading.Thread(target=pump_3_thread_task)
 
-        def pump_1_thread():
-            self.oldVal1 = self.curVal1
-            self.curVal1 = self.gui.c1.get()
-            self.deltVal1 = self.curVal1 - self.oldVal1
-            self.deltVol1 = 0.01 * self.deltVal1 * float(self.gui.maxvol_entry.get())
+        #start thread
+        pump1_thread.start()
+        pump2_thread.start()
+        pump3_thread.start()
 
-
-
-
+        #wait until all threads done
+        pump1_thread.join()
+        pump2_thread.join()
+        pump3_thread.join()
 
                            
     #checks all pumps when check pump button pressed
@@ -126,3 +145,92 @@ class Backend:
         Pump1.check_pump("11 PICO PLUS ELITE 3.0.7")
         Pump2.check_pump("11 PICO PLUS ELITE 3.0.7")
         Pump3.check_pump("11 PICO PLUS ELITE 3.0.7")
+
+
+
+'''
+
+def buttonPush(self):
+
+    # Slide all the current slider values to the old slider values
+    self.oldVal1 = self.curVal1
+    self.oldVal2 = self.curVal2
+    self.oldVal3 = self.curVal3
+
+    # Set all current slider values equal to values from the GUI
+    self.curVal1 = self.gui.c1.get()
+    self.curVal2 = self.gui.c2.get()
+    self.curVal3 = self.gui.c3.get()
+
+    # Set the values for percent change in volume from each slider
+    self.deltVal1 = self.curVal1 - self.oldVal1
+    self.deltVal2 = self.curVal2 - self.oldVal2
+    self.deltVal3 = self.curVal3 - self.oldVal3
+
+    # Multiply by the max volume per channel and divide by 100 to get the absolute
+    # change in volume for each channel
+    self.deltVol1 = 0.01 * self.deltVal1 * float(self.gui.maxvol_entry.get())
+    self.deltVol2 = 0.01 * self.deltVal2 * float(self.gui.maxvol_entry.get())
+    self.deltVol3 = 0.01 * self.deltVal3 * float(self.gui.maxvol_entry.get())
+
+    # Define other GUI parameters
+    self.rate = float(self.gui.rate_entry.get())
+    self.vol = float(self.gui.vol_entry.get())
+    self.diam = float(self.gui.diam_entry.get())
+
+    # Print volume changes for each channel as a sanity check
+    print(str(self.deltVol1), str(self.deltVol2), str(self.deltVol3))
+
+    # Set the pump parameters for Pump 1
+    Pump1.c_volume()
+    Pump1.ci_volume()
+    Pump1.cw_volume()
+    Pump1.syringe_vol(str(self.vol))
+    Pump1.syringe_diam(str(self.diam))
+    Pump1.infuse_rate(str(self.rate), "ml/min")
+    Pump1.withdraw_rate(str(self.rate), "ml/min")
+    Pump1.target_volume(str(abs(self.deltVol1)), "ml")
+
+    # Set the pump parameters for Pump 2
+    Pump2.c_volume()
+    Pump2.ci_volume()
+    Pump2.cw_volume()
+    Pump2.syringe_vol(str(self.vol))
+    Pump2.syringe_diam(str(self.diam))
+    Pump2.infuse_rate(str(self.rate), "ml/min")
+    Pump2.withdraw_rate(str(self.rate), "ml/min")
+    Pump2.target_volume(str(abs(self.deltVol2)), "ml")
+
+    # Set the pump parameters for Pump 3
+    Pump3.c_volume()
+    Pump3.ci_volume()
+    Pump3.cw_volume()
+    Pump3.syringe_vol(str(self.vol))
+    Pump3.syringe_diam(str(self.diam))
+    Pump3.infuse_rate(str(self.rate), "ml/min")
+    Pump3.withdraw_rate(str(self.rate), "ml/min")
+    Pump3.target_volume(str(abs(self.deltVol3)), "ml")
+
+    # Actuate each pump
+    if self.deltVol1 < 0:
+        Pump1.withdraw_pump()
+    elif self.deltVol1 > 0:
+        Pump1.infuse_pump()
+    else:
+        Pump1.stop_pump()
+
+    if self.deltVol2 < 0:
+        Pump2.withdraw_pump()
+    elif self.deltVol2 > 0:
+        Pump2.infuse_pump()
+    else:
+        Pump2.stop_pump()
+
+    if self.deltVol3 < 0:
+        Pump3.withdraw_pump()
+    elif self.deltVol3 > 0:
+        Pump3.infuse_pump()
+    else:
+        Pump3.stop_pump()
+
+'''
